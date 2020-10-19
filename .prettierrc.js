f5a9f5758bbdc8205be899e1b7b71602b2ee5682
Brendan <eggers.brendan@gmail.com>
2020-10-19 19:04:35 -0400

    Adding prettier and eslint and a new boolean to keep it from sending duplicate messages, but also not missing one

---
+++ .prettierrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  // https://prettier.io/docs/en/options.html#print-width
+  printWidth: 80, // default: 80
+
+  // https://prettier.io/docs/en/options.html#tab-width
+  tabWidth: 2, // default: 2
+
+  // https://prettier.io/docs/en/options.html#tabs
+  useTabs: false, // default: false
+
+  // https://prettier.io/docs/en/options.html#semicolons
+  semi: true, // default: true
+
+  // https://prettier.io/docs/en/options.html#quotes
+  singleQuote: true, // default: false
+
+  // https://prettier.io/docs/en/options.html#quote-props
+  quoteProps: 'consistent', // default: 'as-needed'
+
+  // https://prettier.io/docs/en/options.html#jsx-quotes
+  jsxSingleQuote: false, // default: false
+
+  // https://prettier.io/docs/en/options.html#trailing-commas
+  trailingComma: 'all', // default: es5
+
+  // https://prettier.io/docs/en/options.html#bracket-spacing
+  bracketSpacing: true, // default: true
+
+  // https://prettier.io/docs/en/options.html#jsx-brackets
+  jsxBracketSameLine: false, // default: false
+
+  // https://prettier.io/docs/en/options.html#arrow-function-parentheses
+  arrowParens: 'always', // default: always
+
+  // https://prettier.io/docs/en/options.html#range
+  rangeStart: 0, // default: 0
+  rangeEnd: Infinity, // default: Infinity
+
+  // https://prettier.io/docs/en/options.html#require-pragma
+  requirePragma: false, // default: false
+
+  // https://prettier.io/docs/en/options.html#insert-pragma
+  insertPragma: false, // default: false
+
+  // https://prettier.io/docs/en/options.html#prose-wrap
+  proseWrap: 'preserve', // default: preserve
+
+  // https://prettier.io/docs/en/options.html#html-whitespace-sensitivity
+  htmlWhitespaceSensitivity: 'css', // default: css
+
+  // https://prettier.io/docs/en/options.html#end-of-line
+  endOfLine: 'lf', // default: lf
+
+  // https://prettier.io/docs/en/options.html#embedded-language-formatting
+  embeddedLanguageFormatting: 'auto', // default: auto
+};
--- reddit/index.js
+++ reddit/index.js
@@ -17,12 +17,17 @@ const reddit = new snoowrap({
  * @param {boolean} isSubmission - The boolean value of whether or not the reported item is a submission or comment 
  * @param {object} newReport - Reported Item Information
  */
-const insertReportedItem = async (collection, isSubmission, newReport) => {
-  const { id, author, permalink } = newReport;
+const insertReportedItem = async (collection, isSubmission, reportData) => {
+  const { id, author, permalink } = reportData;
+  let success = false;
+  try {
+    await fireNewReportHook(isSubmission, reportData);
+    success = true;
+  } catch(error) {
+    success = false;
+  }
   // Throw new item in the DB
-  await collection.insertOne({ _id: id, author: author.name, permalink, isSubmission }, { forceServerObjectId: false });
-  // Send discord message that it's a newly reported item
-  return fireNewReportHook(isSubmission, newReport);
+  return await collection.findOneAndUpdate({ _id: id }, { $set: { _id: id, author: author.name, permalink, isSubmission, messageSent: success }}, {upsert: true})
 }
 
 /**
@@ -35,10 +40,17 @@ const checkForNewReports = async (modqueue) => {
     // Changing reported ID key to be _id to match MongoDB value
     const { id: _id } = reported_item;
     const isSubmission = Boolean(reported_item.comments);
-    const isReportInDB = await Boolean(collection.findOne({ _id }));
-    if(!isReportInDB) await insertReportedItem(collection, isSubmission, { ...reported_item });
-    // if reported item is already in the database, just ignore and move on
-    return console.log('modqueue filled with old reports');
+    const foundReportedItem = await collection.findOne({ _id });
+
+    // Check if the item exists. If it does not, send message and add it
+    if(!Boolean(foundReportedItem)) return await insertReportedItem(collection, isSubmission, { ...reported_item });
+    // If it does exist, maybe it missed a message so we have to send a new one and update the object
+    if(Boolean(foundReportedItem) && !foundReportedItem.messageSent) {
+      return await insertReportedItem(collection, isSubmission, { ...reported_item });
+    }  
+
+    // if reported item is already in the database and sent, just ignore and move on
+    return;
   }); 
 }
 
@@ -48,7 +60,7 @@ const checkForNewReports = async (modqueue) => {
  */
 const startReportScan = async () => {
   const modqueue = await reddit.getSubreddit(subreddit).getModqueue();
-  if(modqueue.length > 0) await checkForNewReports(modqueue);
+  if(modqueue.length > 0) return await checkForNewReports(modqueue);
   return console.log('nothing there. Script complete');
 }
 
