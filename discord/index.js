e08064d3c22112be8f47061a1fd29b4853fbfb20
Callum C <mrdeadguy34@users.github.com>
2020-11-12 11:44:37 +1300

    Added generic send hook function to send a message

--- discord/index.js
+++ discord/index.js
@@ -3,16 +3,25 @@ const { webhook_url } = require('../secrets.json');
 
 const hook = new Webhook(webhook_url);
 
+const sendHook = async (title, url, fields) => {
+    const embed = new MessageBuilder()
+      .setTitle(title)
+      .setURL(url);
+
+    for (let title in fields) {
+        embed.addField(title, fields[title]);
+    }
+
+    hook.send(embed);
+
+}
+
 const fireNewReportHook = async (isSubmission, reported_item) => {
   const { permalink, author, title, body } = reported_item;
   const messageText = isSubmission ? title : body;
 
-  const embed = new MessageBuilder()
-    .setTitle(`${isSubmission ? 'Post' : 'Comment'} Reported`)
-    .setURL(`https://reddit.com${permalink}`)
-    .addField(author.name, messageText);
-
-  hook.send(embed);
+  sendHook(`${isSubmission ? 'Post' : 'Comment'} Reported`,
+   `https://reddit.com${permalink}`, {author.name: messageText})
 };
 
 module.exports = {
