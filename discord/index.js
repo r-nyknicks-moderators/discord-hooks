f4714dc51ae452ab38258ae94745bee356bcbb8c
Callum C <cpcooper34@hotmail.com>
2020-11-16 13:47:49 +1300

    fix: Fixed bot not initialising properly

--- discord/index.js
+++ discord/index.js
@@ -2,6 +2,17 @@ const { NodeJSBot } = require("nodejsdiscordbot");
 
 const KnicksDiscordBot = new NodeJSBot(process.env.BOT_PREFIX);
 
+const initialiseBot = async (token) => {
+    KnicksDiscordBot.once("ready", () => {
+        KnicksDiscordBot.commandCollection.loadCommands(process.cwd() + "/discord/commands");
+        console.log("Discord Bot ready...");
+    });
+
+    return await KnicksDiscordBot.initialise(token);
+}
+
+initialiseBot(process.env.BOT_TOKEN);
+
 module.exports = {
     KnicksDiscordBot
 };
