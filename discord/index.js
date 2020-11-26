d77c47603e1ba3699fe7974cc60557f751ab46ba
Callum C <cpcooper34@hotmail.com>
2020-11-26 13:12:07 +1300

    fix: removed initialisation of bot and created a class for the bot to be expanded

--- discord/index.js
+++ discord/index.js
@@ -1,17 +1,14 @@
 const { NodeJSBot } = require("nodejsdiscordbot");
 
-const KnicksDiscordBot = new NodeJSBot(process.env.BOT_PREFIX);
-
-const initialiseBot = async (token) => {
-    KnicksDiscordBot.once("ready", () => {
-        KnicksDiscordBot.commandCollection.loadCommands(process.cwd() + "/discord/commands");
-        console.log("Discord Bot ready...");
-    });
-
-    return await KnicksDiscordBot.initialise(token);
+const KnicksDiscordBot = class KnicksDiscordBot extends NodeJSBot {
+    constructor(prefix, options = {}) {
+        super(prefix, options);
+        this.once("ready", () => {
+            KnicksDiscordBot.commandCollection.loadCommands(process.cwd() + "/discord/commands");
+            console.log("Discord Bot ready...");
+        })
+    }
 }
-
-initialiseBot(process.env.BOT_TOKEN);
 
 module.exports = {
     KnicksDiscordBot
