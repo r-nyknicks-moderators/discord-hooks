029a69a94a90cf07261c1d36c3af9a98bb3dddb7
Callum C <cpcooper34@hotmail.com>
2020-11-26 13:11:35 +1300

    fix: reworked codebase into a class

--- reddit/index.js
+++ reddit/index.js
@@ -1,105 +1,113 @@
 const snoowrap = require('snoowrap');
+const { SubmissionStream, CommentStream, ModQueueStream } = require("snoostorm");
 const { KnicksDiscordBot } = require("../discord");
 
-// const { fireNewReportHook } = require('../discord');
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const refreshToken = process.env.REFRESH_TOKEN;
 const subreddit = process.env.SUBREDDIT;
 const badSourceFlairID = process.env.BAD_SOURCE_FLAIR_ID;
 
-//Temporary until db is running
-const linksList = [
+const KnicksRedditBot = class KnicksRedditBot extends snoowrap {
+ /**
+  * Constructs a new instance.
+  *
+  * @param      {string}  clientId                 The client identifier
+  * @param      {string}  clientSecret             The client secret
+  * @param      {string}  refreshToken             The refresh token
+  * @param      {string}  [userAgent="knicksbot"]  The user agent
+  */
+  constructor (clientId, clientSecret, refreshToken, userAgent = "knicksbot") {
+    super({
+      userAgent,
+      clientId,
+      clientSecret,
+      refreshToken
+    });
 
-]
+    //Temporary until database set up
+    this._linksList = []
 
-const reddit = new snoowrap({
-  userAgent: 'knicksbot',
-  clientId,
-  clientSecret,
-  refreshToken,
-});
+    //Discord bot setup
+    this.discordBot = new KnicksDiscordBot(process.env.BOT_TOKEN);
+    this.discordBot.initialise(process.env.BOT_TOKEN);
+    this..commandCollection.on(
+      "ran", this.handleDiscordCommands);
 
-/**
- * Function to handle the commands the discord bot is given
- *
- * @param      {Message}  ctx      The context
- * @param      {Array}  args     The arguments
- * @param      {Return value}  result   The result
- * @param      {Command}  command  The command
- */
-const handleDiscordCommands = async (ctx, args, result, command) => {
-  if (command.name == "delete") {
-    return reddit.getSubmission(args[0]).delete();
+    //Snoostorm streams setup
+    this.modQueueStream = new ModQueueStream(client, {
+      subreddit,
+      limit: 50,
+      pollTime: 2000
+    });
+    this.modQueueStream.on("item", this.checkSubmission);
+
   }
-}
 
-/**
- *
- * @param {string} url - Url to be checked against mongodb list
- * @returns {boolean} - The boolean value of whether or not the link is allowed
- */
-const checkLink = async(url) => {
-  //linksCollection = await connectToDatabase("disallowed_links");
-  //linksList await linksCollection.distinct("domain");
+  /**
+   * Function to handle the commands the discord bot is given
+   *
+   * @param      {Message}  ctx      The context
+   * @param      {Array}  args     The arguments
+   * @param      {Return value}  result   The result
+   * @param      {Command}  command  The command
+   */
+  handleDiscordCommands = async (ctx, args, result, command) => {
+    if (command.name == "delete") {
+      return reddit.getSubmission(args[0]).delete();
+    }
+  }
 
-  let domain = new URL(url).hostname
-  if (linksList.includes(domain)) return false;
-  return true;
-}
+  /**
+   * Checks the url against allowed urls
+   * @param {string} url - Url to be checked against mongodb list
+   * @returns {boolean} - The boolean value of whether or not the link is allowed
+   */
+  checkUrl(url) {
+    //TODO (Callum) : Add special checking for twitter links
+    let domain = new URL(url).hostname
+    if (this._linksList.includes(domain)) return false;
+    return true;
+  }
 
-const checkForNewReports = async (modqueue) => {
-  return modqueue.forEach(async (reported_item) => {
-    const isSubmission = Boolean(reported_item.comments);
+  /**
+   * Checks all submissions
+   *
+   * @param      {snoowrap.submission | snoowrap.comment}  reportedItem  The reported item
+   */
+  checkSubmission(reportedItem) {
+    const isSubmission = Boolean(reportedItem.comments);
 
-    //Run checks on submissions
+      //Run checks on submissions
     if (isSubmission) {
       //TODO (Callum) : Check whether flair is already assigned
-      let linkAllowed = await checkLink(reported_item.url);
+      let linkAllowed = await checkLink(reportedItem.url);
       // Assign bad source flair to invalid sources
       if (!linkAllowed) {
-        reported_item.selectFlair({flair_template_id: badSourceFlairID});
+        reportedItem.selectFlair({flair_template_id: badSourceFlairID});
         KnicksDiscordBot.channels.cache.get(process.env.BOT_SEND_CHANNEL)
         .send({"embed": {
           title:"Bad source found",
-          url: `https://reddit.com${reported_item.permalink}`,
+          url: `https://reddit.com${reportedItem.permalink}`,
           description: "A post with a bad source has been reported",
           fields: [
           {
             name: "Url reported:",
-            value: reported_item.url
+            value: reportedItem.url
           },
           {
             name: "Post ID",
-            value: reported_item.id
+            value: reportedItem.id
           }
           ],
-          timestamp: new Date(reported_item.created_utc * 1000)
+          timestamp: new Date(reportedItem.created_utc * 1000)
         }});
       }    
-    }
-
-  });
+    }    
+  }
 }
 
 
-/**
- * Starts the scan process of checking for new reported items
- */
-const startReportScan = () => {
-  return new Promise(async (resolve, reject) => {
-    const modqueue = await reddit.getSubreddit(subreddit).getModqueue();
-    if (modqueue.length > 0) return resolve(await checkForNewReports(modqueue));
-    return reject(false);
-  });
-};
-
-//Set up discord handler
-KnicksDiscordBot.commandCollection.on(
-  "ran", handleDiscordCommands);
-
-
 module.exports = {
-  reddit,
-  startReportScan,
-};
+  KnicksRedditBot
+}
