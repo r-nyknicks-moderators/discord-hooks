d4f4e0f467e1506428a5fb8dcc40a9fa0a671e3a
Callum C <cpcooper34@hotmail.com>
2020-11-26 14:46:28 +1300

    feat: reworked the bot being run

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 dotenv.config();
-const { startReportScan } = require('./reddit');
+const { KnicksRedditBot } = require('./reddit');
 
 const port = process.env.PORT || 5000;
 
+//Bot setup
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const refreshToken = process.env.REFRESH_TOKEN;
+const subreddit = process.env.SUBREDDIT;
+const badSourceFlairID = process.env.BAD_SOURCE_FLAIR_ID;
+
+bot = new KnicksRedditBot(clientId, clientSecret, refreshToken)
+
 app.listen(port, async () => {
-  await startReportScan()
-    .then(() => {
-      console.log('script complete');
-    })
-    .catch((err) => {
-      console.log('no reports. script complete.');
-    });
+  console.log("bot is running...");
 });
