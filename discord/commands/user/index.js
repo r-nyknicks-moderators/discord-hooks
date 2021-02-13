b108e2b11cd5341154eeb820a9b3283bc8233715
Callum C <cpcooper34@hotmail.com>
2021-02-14 12:29:22 +1300

    feat: Added beginnings of get user command

---
+++ discord/commands/user/index.js
@@ -0,0 +1,14 @@
+const { Command } = require('nodejsdiscordbot');
+const { getUser } = require('../../../database');
+
+module.exports = class GetUser extends Command {
+  constructor() {
+    super('user', ['name'], ['ADMINISTRATOR']);
+  }
+
+  async _run(ctx, args) {
+    const user = await getUser(args[0]);
+    console.log(user);
+    ctx.channel.send(`\`\`\`${JSON.stringify(user)}\`\`\``);
+  }
+};
