4307064feda4b63fd989e06e706c15defb28424e
Callum C <cpcooper34@hotmail.com>
2021-02-14 12:28:51 +1300

    feat: Added database update for reported posts

--- database/index.js
+++ database/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 const { DB_USERNAME, DB_PASSWORD, DB_URL, DB_NAME, SUBREDDIT } = process.env;
 
@@ -7,17 +8,51 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+//Connection has to stay open otherwise JS gets upset
+client.connect();
 
-const insertOrUpdateReport = async ({ id, author, isComment }) => {
-  await client.connect((err) => {
-    const collection = client.db(SUBREDDIT).collection('reports');
-    // collection.find({ id }, (err, res) => console.log(res));
-    // collection.insertOne({ id, author: author.name, isComment }, (err, res) =>
-    //   console.log(err, res),
-    // );
-
-    client.close();
-  });
+const insertOrUpdateReport = async ({
+  id,
+  author,
+  comments,
+  user_reports,
+  mod_reports,
+}) => {
+  //Set up post vars
+  // const isSubmission = Boolean(comments);
+  // const reports = user_reports.length + mod_reports.length;
+  // const name = author.name;
+  // const post = { id, isSubmission, reports}
+  // //Update user reports
+  // const collection = await client.db(SUBREDDIT).collection('user');
+  // const user = await collection.findOne({ name });
+  // if (user) {
+  //   const dbPost = user.reportedPosts.find( (o, i) => {
+  //     if (o.id == id) {
+  //       user.reportedPosts[i] = post;
+  //       return true;
+  //     }
+  //   });
+  //   if (!dbPost) user.reportedPosts.push(post);
+  //   await collection.replaceOne({ name }, user, (err, res) =>
+  //     console.log(err, res),
+  //   );
+  // }
+  // else {
+  //   const newUser = {
+  //     name,
+  //     reportedPosts: [ post ]
+  //   }
+  //   await collection.insertOne(newUser, (err, res) =>
+  //     console.log(err, res),
+  //   );
+  // }
+  // return true;
 };
 
-module.exports = { insertOrUpdateReport };
+const getUser = async (name) => {
+  const collection = await client.db(SUBREDDIT).collection('user');
+  return await collection.findOne({ name });
+};
+
+module.exports = { insertOrUpdateReport, getUser };
