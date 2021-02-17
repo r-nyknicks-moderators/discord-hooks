786d204a129fdf32574096138d6ffe24eb519a4a
Callum C <cpcooper34@hotmail.com>
2021-02-17 17:33:49 +1300

    Feat: sent user's post in a formatted message

--- discord/commands/user/index.js
+++ discord/commands/user/index.js
@@ -5,11 +5,25 @@ module.exports = class GetUser extends Command {
   constructor() {
     super('user', ['name'], ['ADMINISTRATOR']);
   }
-
+  //Fetches all user reports and info
   async _run(ctx, args) {
     const user = await getUser(args[0]);
-    //TODO(Callum): Parse results
-    console.log(user);
-    ctx.channel.send(`\`\`\`${JSON.stringify(user)}\`\`\``);
+    //Set up all posts
+    var posts = [];
+    for (let post of user) {
+      posts.push({
+        name: `https://reddit.com/${post.id}`,
+        value: `Reports: ${post.reports}`,
+      });
+    }
+    //Send as formatted message
+    ctx.channel.send({
+      embed: {
+        title: user[0].author,
+        url: `https://reddit.com/u/${user[0].author}`,
+        description: 'Users reported posts:',
+        fields: posts,
+      },
+    });
   }
 };
